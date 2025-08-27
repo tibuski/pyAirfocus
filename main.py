919738ec29adf9aca8e34e2d0151a7641d3b68ae
tibuski <laurent.brichet@gmail.com>
2025-08-27 09:29:51 +0200

    third commit

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ import httpx
 URL = 'https://app.airfocus.com/api/'
 
 def airfocusApi(URL,endpoint):
-    r = httpx.get(f"{URL}{endpoint}")
-    print(r.status_code)
+    url = f"{URL}{endpoint}"
+    token = os.environ.get('AIRFOCUS_KEY')
+    headers = {"Authorization": f"Bearer {token}"}
+    print(f"{url}\n{token}\n{headers}")
+    r = httpx.get(url, headers=headers)
+    print(r.text)
 
 def main():
-    airfocusApi(URL,'workspaces')
+    print("Environment variable AIRFOCUS_KEY needs to be set !")
+    airfocusApi(URL,'workspaces/count')
 
 if __name__ == "__main__":
     main()
+
